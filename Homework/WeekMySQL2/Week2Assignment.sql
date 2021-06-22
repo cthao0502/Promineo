ac7307e67440f265ae1f9238410b8c556ad91d94
cthao0502 <82789560+cthao0502@users.noreply.github.com>
2021-06-21 21:06:49 -0500

    Update Week2Assignment.sql

--- Homework/WeekMySQL2/Week2Assignment.sql
+++ Homework/WeekMySQL2/Week2Assignment.sql
@@ -1,4 +1,4 @@
-//Question 1:
+#Question 1:
 
 select titles.*, count(*)
 from employees
@@ -7,14 +7,14 @@ where birth_date > '1965-01-01'
 group by titles.title
 order by count(*) desc;
 
-//Question 2:
+#Question 2:
 
 select title as "Titles", avg(salary) as "Average Salary"
 from salaries
 inner join titles on salaries.emp_no = titles.emp_no
 group by (title);
 
-//Question 3:
+#Question 3:
 
 select departments.dept_name, sum(salary)
 from dept_emp
